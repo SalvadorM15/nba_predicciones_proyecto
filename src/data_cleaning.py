33857e55af1e9dbc9c1ac81566a604b0288e894c
salvadorm15 <salvadormunduteguy78@gmail.com>
2026-01-30 19:20:00 -0300

    comienzo de la ingenieria de caracteristicas

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -47,7 +47,6 @@ def gameLog_csv_filter(rawPath, procesedPath):
     campos_numericos = ["FGM", "FGA", "FG_PCT", "FG3M", "FG3_PCT", "FTM", "FTA", "FT_PCT", "OREB", "DREB", "AST", "STL", "BLK", "PF", "PTS"]
     gameLog_df[campos_numericos] = gameLog_df[campos_numericos].apply(pd.to_numeric,errors = "coerce")
 
-    gameLog_df = gamelog_purification(gameLog_df)
     gameLog_df = final_stats_validation(gameLog_df)
 
     final_csv = gameLog_df.to_csv(procesedPath,index=False)
@@ -56,75 +55,6 @@ def gameLog_csv_filter(rawPath, procesedPath):
 
 
 
-
-
-
-
-
-
-
-def gamelog_purification (original_gameLog):
-
-    #buscamos crear un nuevo dataframe purificado a partir de las columnas que seleccionamos
-    #la funcion va a devolver un dataFrame el cual sera el que queda persistido y con el cual trabajaremos en la seccion de feature_eng
-    
-    """
-    gamelog final structure:
-    
-        gamelog_df (
-
-            Local_team_id,
-            visitor_team_id,
-            game_id,
-            visitor_scoring_stats,
-            local_scoring_stats,
-            visitor_defensive_stats,
-            local_defensive_stats,
-        )
-    """
-
-    gameLog_groupby = original_gameLog.groupby("Game_ID")
-    rows = []
-
-    for game_id, teams in gameLog_groupby:
-        if len(teams) == 2:
-            home = teams[teams["MATCHUP"].str.contains("vs").astype(int) == 1].iloc[0]
-            away = teams[teams["MATCHUP"].str.contains("vs").astype(int) == 0].iloc[0]
-
-            rows.append(
-                {
-                    "Game_id": game_id,
-                    "Local_team_id": home["Team_ID"],
-                    "Visitor_team_id": away["Team_ID"],
-                    "Visitor_WL": away["WL"],\
-                    "Local_WL": home["WL"],
-                    "Visitor_FgPct": away["FG_PCT"],
-                    "Local_FgPct": home["FG_PCT"],
-                    "Visitor_FG3aPct": away["FG3_PCT"],
-                    "Local_FG3aPct": home["FG3_PCT"],
-                    "Visitor_FtPct": away["FT_PCT"],
-                    "Local_FtPct": home["FT_PCT"],
-                    "Visitor_AST": away["AST"],
-                    "Local_AST": home["AST"],
-                    "Visitor_OREB": away["OREB"],
-                    "Local_OREB": home["OREB"],
-                    "Visitor_DREB": away["DREB"],
-                    "Local_DREB": home["DREB"],
-                    "Visitor_STL": away["STL"],
-                    "Local_STL": home["STL"],
-                    "Visitor_BLK": away["BLK"],
-                    "Local_BLK": home["BLK"],
-                    "Visitor_PF": away["PF"],
-                    "Local_PF": home["PF"],
-                    "Visitor_PTS": away["PTS"],
-                    "Local_PTS": home["PTS"],
-                }
-            )
-
-
-    final_gameLogs_df = pd.DataFrame(rows)
-    return final_gameLogs_df
-    #
 
 
 def final_stats_validation(gameLog_df):
@@ -148,5 +78,15 @@ def TeamStatistics_csv_filter(rawPath, procesedPath):
 
     treatment_statistics_df = pd.read_csv(rawPath)
 
-    campos_iniciales = ["TEAM_ID","TEAM_NAME","W","L","W_PTC","FGA","FTA","OREB",]
+    campos_iniciales = ["TEAM_ID","TEAM_NAME","W","L","W_PTC","FGA","FTA","OREB","DREB",]
+    campos_numericos = ["FGA","FTA","OREB","DREB","W","L","W_PTC"]
+
+    final_df = treatment_statistics_df[campos_iniciales]
+    final_df[campos_numericos] = final_df[campos_numericos].apply(pd.to_numeric,errors = "coerce")
+    final_df = final_df.drop_duplicates(subset = ["TEAM_ID"])
+
+    final_csv = final_df.to_csv(procesedPath,index=False)
     
+    return final_df
+
+
